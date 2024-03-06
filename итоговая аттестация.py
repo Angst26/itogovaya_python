8dd6803b561eb7ce237938fb9be2e3615eecd49a
Yuriy Kozhin <ukozin36@gmail.com>
2024-03-06 10:11:24 +0300

    done

---
+++ итоговая аттестация.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+import random
+
+lst = ["robot"] * 10
+lst += ["human"] * 10
+random.shuffle(lst)
+data = pd.DataFrame({"whoAmI": lst})
+
+data["robot"] = (data["whoAmI"] == "robot").astype(int)
+data["human"] = (data["whoAmI"] == "human").astype(int)
+
+
+print(data.head(10))
